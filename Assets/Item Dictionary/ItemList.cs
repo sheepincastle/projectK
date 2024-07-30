ac1150cbef91243907bc2c84fd85204aad693ade
NERU <park.seyeon0613@gmail.com>
2024-07-31 01:46:02 +0900

    아이템 값 정리 시스템 업데이트
    power, defense, attackSpeed, duration, itemType 함수 추가와 아이템을 타입별로 구분할수있게 업데이트
    ex) Weapon,Armor,Potion

--- Assets/Item Dictionary/ItemList.cs
+++ Assets/Item Dictionary/ItemList.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Item{
+public class Item
+{
     public string itemName;
     public Sprite icon;
     public int value;
+    public int power;
+    public int defense;
+    public int attackSpeed;
+    public int duration;
+    public ItemType itemType; // 추가된 필드
 
-    public Item(string name, Sprite icon, int value)
+    public Item(string name, Sprite icon, int value, int power, int defense, int attackSpeed, int duration, ItemType itemType)
     {
-     this.itemName = name;
-     this.icon = icon;
-     this.value = value;
+        this.itemName = name;
+        this.icon = icon;
+        this.value = value;
+        this.power = power;
+        this.defense = defense;
+        this.attackSpeed = attackSpeed;
+        this.duration = duration;
+        this.itemType = itemType; // 추가된 초기화 코드
     }
 }
+
+public enum ItemType
+{
+    Weapon,
+    Armor,
+    Potion
+}
+
+
--- Assets/Item Dictionary/ItemManager.cs
+++ Assets/Item Dictionary/ItemManager.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
-    public List<Item> itemList; // 아이템을 추가할 리스트
-    private Dictionary<string, Item> itemDictionary; // 아이템을 관리할 사전
+    public List<Item> itemList = new List<Item>(); // 아이템을 추가할 리스트
+    private Dictionary<string, Item> itemDictionary = new Dictionary<string, Item>(); // 아이템을 관리할 사전
 
     void Start()
     {
-        itemDictionary = new Dictionary<string, Item>();
-
         // 리스트에 있는 아이템들을 사전에 추가
         foreach (Item item in itemList)
         {
@@ -19,27 +17,17 @@ public class ItemManager : MonoBehaviour
                 itemDictionary.Add(item.itemName, item);
             }
         }
-
-        // 예제: 아이템 이름으로 아이템 검색
-        string searchName = "Health Potion";
-        if (itemDictionary.TryGetValue(searchName, out Item foundItem))
-        {
-            Debug.Log("Item found: " + foundItem.itemName + " Value: " + foundItem.value);
-        }
-        else
-        {
-            Debug.Log("Item not found: " + searchName);
-        }
     }
 
     // 새로운 아이템을 추가하는 메서드
-    public void AddItem(string itemName, Sprite icon, int value)
+    public void AddItem(string itemName, Sprite icon, int value, int power, int defense, int attackSpeed, int duration, ItemType itemType)
     {
         if (!itemDictionary.ContainsKey(itemName))
         {
-            Item newItem = new Item(itemName, icon, value);
+            Item newItem = new Item(itemName, icon, value, power, defense, attackSpeed, duration, itemType);
             itemList.Add(newItem);
             itemDictionary.Add(itemName, newItem);
+            Debug.Log("Item added: " + itemName);
         }
         else
         {
@@ -55,11 +43,56 @@ public class ItemManager : MonoBehaviour
             Item itemToRemove = itemDictionary[itemName];
             itemList.Remove(itemToRemove);
             itemDictionary.Remove(itemName);
+            Debug.Log("Item removed: " + itemName);
         }
         else
         {
             Debug.Log("Item not found: " + itemName);
         }
     }
-}
 
+    // 아이템을 이름으로 검색하는 메서드
+    public Item GetItem(string itemName)
+    {
+        if (itemDictionary.TryGetValue(itemName, out Item foundItem))
+        {
+            Debug.Log("Item found: " + foundItem.itemName + " Value: " + foundItem.value);
+            return foundItem;
+        }
+        else
+        {
+            Debug.Log("Item not found: " + itemName);
+            return null;
+        }
+    }
+
+    // 아이템 리스트를 반환하는 메서드
+    public List<Item> GetItemList()
+    {
+        return new List<Item>(itemList);
+    }
+
+    // 아이템을 타입별로 분류하는 메서드
+    public void SortItemsByType(out List<Item> weapons, out List<Item> armors, out List<Item> potions)
+    {
+        weapons = new List<Item>();
+        armors = new List<Item>();
+        potions = new List<Item>();
+
+        foreach (Item item in itemList)
+        {
+            switch (item.itemType)
+            {
+                case ItemType.Weapon:
+                    weapons.Add(item);
+                    break;
+                case ItemType.Armor:
+                    armors.Add(item);
+                    break;
+                case ItemType.Potion:
+                    potions.Add(item);
+                    break;
+            }
+        }
+    }
+}
