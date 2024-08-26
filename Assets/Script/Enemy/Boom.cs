ee4b5f60e1adf86ff7d0fd740cb13550917f891f
panpanya0407 <nanenada129@gmail.com>
2024-08-26 22:43:22 +0900

    골렘 스크립트 완

--- Assets/Script/Enemy/Boom.cs
+++ Assets/Script/Enemy/Boom.cs
@@ -14,12 +14,11 @@ public class Boom : MonoBehaviour
     {
         
     }
-    void OnCollisionEnter2D(Collision2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         //플레이어와 접촉시 공격
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("y");
             GameManager.player_current_HP -= boom_damage;
         }
     }
--- Assets/Script/Enemy/Golem.cs
+++ Assets/Script/Enemy/Golem.cs
@@ -13,8 +13,12 @@ public class Golem : MonoBehaviour
     Transform player_transform;
     Player player_script;
     Transform transform;
-    
+
+    bool boom = false;
+    float boom_range = 3;
+    float boom_cooltime = 22;
     float batspwan_cooltime = 0;
+    public GameObject boomPrepab;
     public GameObject batPrepab;
     public int dash_power = 40;
     float dash_speed;
@@ -23,6 +27,7 @@ public class Golem : MonoBehaviour
     bool dash = false;
     private Vector3 offset = new Vector3(0, 1, 0);
     private Vector3 dash_target;
+
     void Start()
     {
         transform = GetComponent<Transform>();
@@ -41,8 +46,10 @@ public class Golem : MonoBehaviour
         dash_cooltime += Time.deltaTime;
         //박쥐소환 쿨타임 갱신
         batspwan_cooltime += Time.deltaTime;
+        //폭발 쿨타임 갱신
+        boom_cooltime += Time.deltaTime;
         //박쥐소환쿨타임이 설정값 이상일때 박쥐소환
-        if(batspwan_cooltime >= 20)
+        if(batspwan_cooltime >= 18 && dash == false && enemyData.enemy_current_HP > 0 && boom == false)
         {
             enemyMove.moveable = false;
             animator.SetTrigger("Ability");
@@ -52,6 +59,15 @@ public class Golem : MonoBehaviour
             GameObject bat3 = Instantiate(batPrepab, new Vector2(transform.position.x + 3, transform.position.y), Quaternion.identity);
             Invoke("movestart", 0.5f);
         }
+        //폭발 쿨타임이 설정값 이상일때 폭발
+        if(boom_cooltime >= 27 && batspwan_cooltime < 18 && batspwan_cooltime > 1 && dash == false && enemyData.enemy_current_HP > 0)
+        {
+            boom = true;
+            enemyMove.moveable = false;
+            animator.SetTrigger("Attack 2");
+            boom_cooltime = 0;
+            Invoke("boom1", 1.2f);  
+        }
         //죽음
         if (enemyData.enemy_current_HP <= 0)
         {
@@ -60,7 +76,7 @@ public class Golem : MonoBehaviour
             Destroy(gameObject, 1);
         }
         //일정거리에 플레이어가 있고 돌진쿨타임이 설정값 이상일때 돌진 준비
-        if(dash_cooltime >= 10 && distance >= 6 && distance <= 12)
+        if(dash_cooltime >= 10 && distance >= 6 && distance <= 12 && enemyData.enemy_current_HP > 0 && boom == false)
         {
             //잠시 정지
             enemyMove.moveable = false;
@@ -119,6 +135,46 @@ public class Golem : MonoBehaviour
     {
         enemyMove.moveable = true;
     }
-    
+    //1차 폭발
+    void boom1()
+    {
+        animator.SetTrigger("Ability");
+        for(int i = 1; i <= boom_range; i++)
+        {
+            GameObject boom1 = Instantiate(boomPrepab, new Vector2(transform.position.x - 5*i, transform.position.y), Quaternion.identity);
+            GameObject boom2 = Instantiate(boomPrepab, new Vector2(transform.position.x + 5*i, transform.position.y), Quaternion.identity);
+            GameObject boom3 = Instantiate(boomPrepab, new Vector2(transform.position.x , transform.position.y - 5*i), Quaternion.identity);
+            GameObject boom4 = Instantiate(boomPrepab, new Vector2(transform.position.x , transform.position.y + 5*i), Quaternion.identity);
+        }
+        Invoke("boom2", 1.2f);
+    }
+    //2차 폭발
+    void boom2()
+    {
+        animator.SetTrigger("Ability");
+        for (int i = 1; i <= boom_range; i++)
+        {
+            GameObject boom1 = Instantiate(boomPrepab, new Vector2(transform.position.x - 3 * i, transform.position.y + 3*i), Quaternion.identity);
+            GameObject boom2 = Instantiate(boomPrepab, new Vector2(transform.position.x - 3 * i, transform.position.y -3*i), Quaternion.identity);
+            GameObject boom3 = Instantiate(boomPrepab, new Vector2(transform.position.x + 3*i, transform.position.y - 3 * i), Quaternion.identity);
+            GameObject boom4 = Instantiate(boomPrepab, new Vector2(transform.position.x + 3 *i, transform.position.y + 3 * i), Quaternion.identity);
+        }
+        Invoke("boom3", 1.2f);
+    }
+    //3차 폭발
+    void boom3()
+    {
+        animator.SetTrigger("Ability");
+        GameObject boom1 = Instantiate(boomPrepab, new Vector2(transform.position.x - 5 , transform.position.y), Quaternion.identity);
+        GameObject boom2 = Instantiate(boomPrepab, new Vector2(transform.position.x + 5 , transform.position.y), Quaternion.identity);
+        GameObject boom3 = Instantiate(boomPrepab, new Vector2(transform.position.x, transform.position.y - 5 ), Quaternion.identity);
+        GameObject boom4 = Instantiate(boomPrepab, new Vector2(transform.position.x, transform.position.y + 5 ), Quaternion.identity);
+        GameObject boom5 = Instantiate(boomPrepab, new Vector2(transform.position.x - 3 , transform.position.y + 3), Quaternion.identity);
+        GameObject boom6 = Instantiate(boomPrepab, new Vector2(transform.position.x - 3 , transform.position.y - 3 ), Quaternion.identity);
+        GameObject boom7 = Instantiate(boomPrepab, new Vector2(transform.position.x + 3 , transform.position.y - 3 ), Quaternion.identity);
+        GameObject boom8 = Instantiate(boomPrepab, new Vector2(transform.position.x + 3 , transform.position.y + 3 ), Quaternion.identity);
+        Invoke("movestart", 1f);
+        boom = false;
+    }
     
 }
