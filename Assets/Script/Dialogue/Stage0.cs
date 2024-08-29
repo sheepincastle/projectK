25878e80bfe57ddaef8b802f1bfb09fe7e5477aa
Wogml-07 <jaehuibag3@gmail.com>
2024-08-29 19:04:38 +0900

    스토리 대사 추가

--- Assets/Script/Dialogue/Stage0.cs
+++ Assets/Script/Dialogue/Stage0.cs
@@ -12,7 +12,7 @@ public class Stage0 : MonoBehaviour
     void Start()
     {
         stageManager = GetComponent<StageManager>();
-        //튜토이얼 스크립트 출력 전 일시정지
+        //튜토리얼 스크립트 출력 전 일시정지
         stageManager.progress = false;
         PlayerMove.moveable = false;
         StartCoroutine(Tutorial());
@@ -22,15 +22,23 @@ public class Stage0 : MonoBehaviour
     {
         script.SetActive(true);
 
-        text.text = "'wasd'를 통해 움직일 수 있어.";
+        text.text = "균열을 타고 들어오니까 갑자기 미래로 와 버렸어...";
+        PlayerMove.moveable = true;
+        yield return new WaitForSeconds(3f);
+        
+        text.text = " 낯선 사람들에게 납치된 내 동생을 찾아야 해!";
+        PlayerMove.moveable = true;
+        yield return new WaitForSeconds(3f);
+        
+        text.text = "'w a s d'를 통해 움직일 수 있어.";
         PlayerMove.moveable = true;
         yield return new WaitForSeconds(3f);
 
-        text.text = "'tap키'를 통해 내 무기를 바꿀 수 있을거야.";
+        text.text = "'tap키'를 통해 무기를 활로 바꿀 수 있어.";
         yield return new WaitForSeconds(3f);
 
         stageManager.progress = true;//적 소환
-        text.text = "으... 쥐라니, 저런걸 '보면' '상태이상'에 빠지고 말거야.";
+        text.text = "으... 쥐라니, 저런걸 보면 '상태이상'에 빠지고 말거야.";
         yield return new WaitForSeconds(3f);
 
         text.text = "쥐에 부딪히면 '경직'에 걸릴 수도 있어. 조심해.";
